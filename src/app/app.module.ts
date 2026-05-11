35f4f2a973aa026e385f2eaf114c2e45000957c7
Celeste M. Moioli <celeste.moioli@gmail.com>
2026-05-11 11:15:07 -0300

    Personalización de nombres e íconos de las tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
+
+// Inicializamos Firebase globalmente
+initializeApp(environment.firebase);
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
